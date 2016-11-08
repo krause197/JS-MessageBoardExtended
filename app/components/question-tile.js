21a5560eeded57f8f1505eb8c204e3f0e6240904
Eric Krause <krause197@gmail.com>
2016-11-07 20:38:37 -0800

    add functionality addFavorites

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,4 +4,12 @@ export default Ember.Component.extend({
   totalAnswers: Ember.computed('question.answers', function() {
     return 'Total Answers: ' + this.get('question.answers.length');
   }),
+
+  favoriteList: Ember.inject.service(),
+
+  actions: {
+    addFavorite(question) {
+      this.get('favoriteList').add(question);
+    }
+  }
 });
