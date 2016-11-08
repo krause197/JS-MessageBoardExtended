85e08f3d039fc2c4ceaada4ce30a12fc05e4c4df
Eric Krause <krause197@gmail.com>
2016-11-07 20:51:18 -0800

    fixed couple of errors

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -15,6 +15,9 @@ export default Ember.Component.extend({
       };
       this.set('addNewQuestion', false);
       this.sendAction('saveQuestion2', params);
+      this.set('text', '');
+      this.set('author', '');
+      this.set('notes', '');
     }
   }
 });
